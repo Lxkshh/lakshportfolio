5ca603230640f6061775d3bbe3d98d70a07d06c7
Lxkshh <159511550+Lxkshh@users.noreply.github.com>
2024-03-01 11:22:05 +0530

    Add files via upload
    added javascript file

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleHeader() {
+    var header = document.getElementById("header");
+    if (header.style.left === "-250px") {
+      header.style.left = "0";
+    } else {
+      header.style.left = "-250px";
+    }
+  }
+  
